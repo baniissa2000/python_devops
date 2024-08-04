c948283f894e0ff12f515bb3a18b5f015f2f4055
baniissa2000 <97743976+baniissa2000@users.noreply.github.com>
2024-08-04 16:40:32 +0000

    adding needed files

--- Day-01/02-hello-world.py
+++ Day-01/02-hello-world.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+import boto3
+import json
+
+ec2 = boto3.client('ec2')
+
+response = ec2.describe_snapshots(OwnerIds=['self'])
+
+for snapshot in response['Snapshots']:
+    volume_id = snapshot.get('VolumeId')
+    print(snapshot['SnapshotId'], snapshot['Description'], snapshot['VolumeId'], snapshot['VolumeSize'] )
